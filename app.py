7e6846c7bdcde60325c36eac471796adb17735fd
carlosrodriguesjf <carlosrodriguesjf@gmail.com>
2024-10-29 19:45:16 -0300

    29-10-24 - Interface gráfica completada

--- app.py
+++ app.py
@@ -5,52 +5,66 @@ import customtkinter as Ctk
 
 
 
-class ToplevelWindow(Ctk.CTkToplevel):
+class CadastroFuncionarios(Ctk.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         def cadastro_usuarios():
             # Pega todo o texto do textbox
-            texto_cadastro_usuario = textbox__cadastro_usuario.get("1.0", "end").strip()  
-            texto_cadastro_senha = textbox_cadastro_senha.get("1.0", "end").strip()
-            print(f"Funcionário {texto_cadastro_usuario} cadastrado com sucesso")
+            texto_cadastro_usuario = textbox_cadastro_usuario.get("1.0", "end").strip()  
+            texto_cadastro_senha = textbox_cadastro_senha.get().strip()
+
+            # Label teste cadastro com sucesso
+            Ctk.CTkLabel(self, text="Cadastro realizado com sucesso!", fg_color="transparent", text_color="black").pack(pady=10)
             print(texto_cadastro_usuario)
+            print(texto_cadastro_senha)
 
 
-        # def apagar_textbox_cadastro_usuario(evento):
-        #     texto_cadastro_usuario.delete("0.0", "end")
+        def fecha_cadastro_funcionarios():
+        
+            self.destroy()
 
 
-        # def apagar_textbox_cadastro_senha(evento):
-        #     texto_cadastro_senha.delete("0.0", "end")
+        def apagar_textbox_cadastro_usuario(evento):
+            textbox_cadastro_usuario.delete("0.0", "end")
 
 
+        def apagar_textbox_cadastro_senha(evento):
+            textbox_cadastro_senha.delete(0, "end")
+
+
+  
 
         # Configurações da janela
         self.title("Cadastro funcionários")
-        self.geometry("500x600")
+        self.geometry("400x500")
 
         # Caixa de texto nome de usuario
-        textbox__cadastro_usuario = Ctk.CTkTextbox(self, width=200, height=20)
-        textbox__cadastro_usuario.pack(pady=20)
-        textbox__cadastro_usuario.insert("1.0", "Digite seu nome de usuário")
-        #textbox__cadastro_usuario.bind("<FocusIn>", apagar_textbox_cadastro_usuario)
+        textbox_cadastro_usuario = Ctk.CTkTextbox(self, width=200, height=20)
+        textbox_cadastro_usuario.pack(pady=20)
+        textbox_cadastro_usuario.insert("1.0", "Digite seu nome de usuário")
+        textbox_cadastro_usuario.bind("<FocusIn>", apagar_textbox_cadastro_usuario)
 
         # Caixa de texto senha
-        textbox_cadastro_senha = Ctk.CTkTextbox(self, width=200, height=20)
+        textbox_cadastro_senha = Ctk.CTkEntry(self, width=200, height=20, show="*")
         textbox_cadastro_senha.pack(pady=20)
-        textbox_cadastro_senha.insert("1.0", "Digite sua senha")
-        #textbox_cadastro_senha.bind("<FocusIn>", apagar_textbox_cadastro_senha)
-
+        textbox_cadastro_senha.insert(0, "Digite sua senha")
+        textbox_cadastro_senha.bind("<FocusIn>", apagar_textbox_cadastro_senha)
             
         # Botão de confirmação de cadastro
         botao_cadastro = Ctk.CTkButton(self, text="Cadastar", command=cadastro_usuarios)
         botao_cadastro.pack(pady=10)
 
+        # Label link para login
+        login_usuario = Ctk.CTkButton(self, text="Já tem uma conta? Faça o login", fg_color="transparent", text_color="black", hover_color="lightgray", command=fecha_cadastro_funcionarios)
+        login_usuario.pack(pady=20)
+
         self.toplevel_window = None
 
+        
 
-class Login(Ctk.CTk):
+
+class LoginFuncionarios(Ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -58,9 +72,13 @@ class Login(Ctk.CTk):
         def button_callback():
             # Pega todo o texto do textbox
             texto_usuario = textbox_usuario.get("1.0", "end").strip()  
-            texto_senha = textbox_senha.get("1.0", "end").strip()
+            texto_senha = textbox_senha.get().strip()
             print(texto_usuario)
             print(texto_senha)
+
+            # Label teste login sucesso
+            Ctk.CTkLabel(self, text="Login realizado com sucesso!", fg_color="transparent", text_color="black").pack(pady=10)
+           
 
 
         def apagar_textbox_usuario(evento):
@@ -68,13 +86,13 @@ class Login(Ctk.CTk):
 
 
         def apagar_textbox_senha(evento):
-            textbox_senha.delete("0.0", "end")
+            textbox_senha.delete("0", "end")
 
 
 
         # Configurações da janela
         self.title("Login funcionários")
-        self.geometry("400x250")
+        self.geometry("400x320")
 
         # Caixa de texto nome de usuario
         textbox_usuario = Ctk.CTkTextbox(self, width=200, height=20)
@@ -83,9 +101,9 @@ class Login(Ctk.CTk):
         textbox_usuario.bind("<FocusIn>", apagar_textbox_usuario)
 
         # Caixa de texto senha
-        textbox_senha = Ctk.CTkTextbox(self, width=200, height=20)
+        textbox_senha = Ctk.CTkEntry(self, width=200, height=20, show="*")
         textbox_senha.pack(pady=20)
-        textbox_senha.insert("1.0", "Digite sua senha")
+        textbox_senha.insert(0, "Digite sua senha")
         textbox_senha.bind("<FocusIn>", apagar_textbox_senha)
 
             
@@ -93,18 +111,29 @@ class Login(Ctk.CTk):
         botao_cadastro = Ctk.CTkButton(self, text="Login", command=button_callback)
         botao_cadastro.pack(pady=10)
 
-        registro_usuario = Ctk.CTkButton(self, text="Não tem uma conta? Registre-se", fg_color="transparent", text_color="black", command=self.open_toplevel)
+        # Label link para cadastro
+        registro_usuario = Ctk.CTkButton(self, text="Não tem uma conta? Registre-se", fg_color="transparent", text_color="black", hover_color="lightgray", command=self.abre_cadastro_funcionarios)
         registro_usuario.pack(pady=20)
 
         self.toplevel_window = None
 
 
-    def open_toplevel(self):
+    def abre_cadastro_funcionarios(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = ToplevelWindow(self)  # create window if its None or destroyed
+            self.toplevel_window = CadastroFuncionarios(self)  # create window if its None or destroyed
+            self.toplevel_window.attributes("-topmost", True)
+            self.toplevel_window.focus_set()
+            
         else:
-            self.toplevel_window.focus()  # if window exists focus it
+            self.toplevel_window.attributes("-topmost", True) # Garante que a janela fique no topo, se já existir
+            self.toplevel_window.focus_set()  # if window exists focus it
+
+    
 
 
-app = Login()
+app = LoginFuncionarios()
 app.mainloop()
+
+
+
+
